9eb33d34ca4537204f6142726ef802e62ae62441
sergey d <s.dusanyuk@gmail.com>
2021-05-19 03:37:10 +0300

    upd thesaurus_adv_ext

--- sergey_dusanyuk_dz_3/task_3_3.py
+++ sergey_dusanyuk_dz_3/task_3_3.py
@@ -24,17 +24,15 @@ def thesaurus_adv(*args):
 
 def thesaurus_adv_ext(*args):
     result_dict = {}
-    for key_ln in sorted(list(set([full_name.split(' ')[1][0] for full_name in args]))):
-        for key_fn in sorted(list(set([full_name.split(' ')[0][0] for
-                                       full_name in
-                                       sorted(list(filter(lambda full_name:
-                                                          full_name.split(' ')[1].startswith(key_ln),
-                                                          args)))]))):
-            result_dict.setdefault(key_ln, {})[key_fn] = list(
-                filter(lambda full_name: full_name.split(' ')[0].startswith(key_fn),
-                       sorted(list(filter(lambda full_name:
-                                          full_name.split(' ')[1].startswith(key_ln),
-                                          args)))))
+
+    for full_name in sorted(args):
+        result_dict.setdefault(full_name.split(' ')[1][0], {}).\
+            setdefault(full_name.split(' ')[0][0], []).append(full_name)
+
+    for key, rd in result_dict.items():
+        result_dict[key] = dict(sorted(rd.items()))
+
+    result_dict = dict(sorted(result_dict.items()))
     return result_dict
 
 
@@ -43,5 +41,5 @@ if __name__ == '__main__':
     print(names)
     names_adv = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
     print(names_adv)
-    names_adv_ext = thesaurus_adv_ext("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
-    print(names_adv)
+    names_adv_ext = thesaurus_adv_ext("Инна Серова", "Петр Алексеев", "Илья Иванов", "Иван Сергеев", "Анна Савельева")
+    print(names_adv_ext)
