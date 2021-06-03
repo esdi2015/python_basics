ef311dfe3b63298c5bba25f209379103a9485732
sergey d <s.dusanyuk@gmail.com>
2021-06-03 20:16:31 +0300

    removed "1" in the expression (the same issue were in the  task 4, I just forgot remove it in the task 3 automatically)

--- sergey_dusanyuk_dz_7/task_7_3.py
+++ sergey_dusanyuk_dz_7/task_7_3.py
@@ -6,7 +6,7 @@ res_dict = dict()
 
 for root, dirs, files in os.walk(explore_dir):
     for file in files:
-        key = 10**(len(str(os.stat(os.path.join(root, file)).st_size))+1)
+        key = 10**(len(str(os.stat(os.path.join(root, file)).st_size)))
         res_dict[key] = res_dict.get(key, 0) + 1
 
 res_dict = dict(sorted(res_dict.items(), key=lambda item: item[0]))
